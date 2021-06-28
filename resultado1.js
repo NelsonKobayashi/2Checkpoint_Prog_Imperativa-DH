6fde8b97b88864bf7d93d3faa2563a0b5fbc4cb0
nelsonkobayashi <koba.nelson@gmail.com>
2021-06-28 16:37:38 -0300

    atualizando código para melhorar a leitura e otimizando alguns comandos.

--- resultado1.js
+++ resultado1.js
@@ -9,16 +9,18 @@ var pratos = {
 function preparo (comida, tempo){
     if (comida == 'pipoca' || comida == 'macarrao' || comida == 'carne' || 
         comida =='feijao' || comida == 'brigadeiro') {
-            if (tempo == pratos[comida]) { console.log(`${comida} preparado(a), bom apetite!`);
-            } else if (tempo < pratos[comida]) { console.log ('Tempo insuficiente, comida não está pronta.');
-            } else if (tempo > pratos[comida] && (tempo < pratos[comida]*3)) { console.log ('A comida queimou... :(');
-            } else if (tempo > (pratos[comida]*3)) { console.log('KABUUUUUUMMMM !!!! *.*');
+            if (tempo == pratos[comida]){
+                console.log(`${comida} preparado(a), bom apetite!`);
+            } else if (tempo < pratos[comida]) {
+                console.log (`Tempo insuficiente, o(a) ${comida} não está pronto(a).`);} 
+            else if (tempo > pratos[comida] && (tempo < pratos[comida]*3)) {
+                console.log (`O(A) ${comida} queimou... :(`);
+            }
+            else {
+                console.log('KABUUUUUUMMMM !!!! *.*');
             }
     } else { 
         return 'Comida inexistente, tente novamente.'
     }
 }
-preparo ("pipoca", 10);
-preparo ("macarrao", 20);
-preparo ("feijao", 30);
-preparo ("carne", 7);
+preparo ('pipoca', 29);
